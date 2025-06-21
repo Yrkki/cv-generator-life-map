5ffebb4a15b3263a0e0b047705042e783d9b7bc4
Jorich <35774231+Yrkki@users.noreply.github.com>
2025-06-21 19:45:49 +0300

    fix(chart): implicitly import the Plotly(ViaCDN)Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-// Connect Plotly
-import { CommonModule } from '@angular/common';
-import { PlotlyViaCDNModule } from 'angular-plotly.js';
-// PlotlyViaCDNModule.plotlyVersion = 'latest';
-// PlotlyViaCDNModule.plotlyBundle = 'geo';
-
 @NgModule({
   declarations: [],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonModule,
-    PlotlyViaCDNModule
   ],
   providers: [],
   bootstrap: [AppComponent]
